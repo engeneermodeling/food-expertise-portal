b8e1f8b28cf3a4bc582f089bc75dfcff75b0d6c3
TarasMysiuraTutor <tarasmysiuratutor@gmail.com>
2026-06-26 23:09:46 +0300

    Add google-site-verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ export const metadata: Metadata = {
   title: "FoodExpertisePortal",
   description: "Food expertise portal",
   icons: {
-    icon: '/favicon.png',
-    apple: '/apple-touch-icon.png',
+    icon: "/favicon.png",
+    apple: "/apple-touch-icon.png",
   },
 };
 
@@ -19,13 +19,19 @@ export default function RootLayout({
   return (
     <html lang="uk" suppressHydrationWarning>
       <head>
+        <meta
+          name="google-site-verification"
+          content="f5nalxq8R9B9N98asYtQaqbeD03S0T-mX9BgLfKKYe4"
+        />
+
         <link rel="icon" href="/favicon.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
-      <body suppressHydrationWarning className="min-h-screen bg-slate-50 dark:bg-slate-950">
-        <ThemeProvider>
-          {children}
-        </ThemeProvider>
+      <body
+        suppressHydrationWarning
+        className="min-h-screen bg-slate-50 dark:bg-slate-950"
+      >
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );
